37966749566bf25598ed5f7c4bc25819303c07b5
erin johnson <erin.nicole.johnson@gmail.com>
2017-10-14 09:39:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function destructivelyPrependKitten(name) {
   return kittens
 }
 
-function destructivelyRemoveLastKitten() { 
+function destructivelyRemoveLastKitten() {
   kittens.pop;
   return kittens
 }
