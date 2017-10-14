dc2f6f72f41d66c40ff831637d31c8788f26a039
erin johnson <erin.nicole.johnson@gmail.com>
2017-10-14 09:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const app = "I don't do much."
 
 var destructivelyAppendKitten = function(name) {
-  kittens.push;
+  kittens.push(name);
+  return kittens
 }
 
 function destructivelyPrependKitten(name) {
